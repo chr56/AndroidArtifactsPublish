bf3720c2d145825d26e63b240d3750cf887e3298
chr56 <chr0056@gmail.com>
2024-03-05 00:00:46 +0800

    [plugin] check existence of Android Gradle Plugin

--- release-artifacts-publish/src/main/kotlin/tools/release/Plugin.kt
+++ release-artifacts-publish/src/main/kotlin/tools/release/Plugin.kt
@@ -9,9 +9,12 @@ import org.gradle.api.Project
 
 class AndroidArtifactsPublishPlugin : Plugin<Project> {
     override fun apply(target: Project) {
+        target.plugins.findPlugin(ANDROID_GRADLE_PLUGIN)
+            ?: throw RuntimeException("Application Android Gradle Plugin (com.android.application) not exists!")
         target.extensions.create(Constants.EXTENSION, AndroidArtifactsPublishExtension::class.java, target)
     }
 
     companion object {
+        private const val ANDROID_GRADLE_PLUGIN = "com.android.application"
     }
 }
