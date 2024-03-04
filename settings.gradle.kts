853bbea31e1dccf0092b7d081386e356b564d5b8
chr56 <chr0056@gmail.com>
2024-03-04 16:06:39 +0800

    [repo] prepare gradlew 8.5-bin

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+@file:Suppress("UnstableApiUsage")
+
+rootProject.name = "AndroidArtifactsPublish"
+rootProject.buildFileName = "build.gradle.kts"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
